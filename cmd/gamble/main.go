9c47db1e246c68ef21eedd5b8fc8ed2f1326fc23
iyoshiha <yoshi3co@gmail.com>
2023-06-08 23:45:20 +0900

    xerrors

--- cmd/gamble/main.go
+++ cmd/gamble/main.go
@@ -1,8 +1,14 @@
 package main
 
 import (
+	"errors"
+	"fmt"
+	"log"
+
 	"github.com/casino/internal/rule"
+	"golang.org/x/xerrors"
 )
+
 
 type result struct{
 	wins int
@@ -11,23 +17,40 @@ type result struct{
 }
 
 func main(){
-	
+	res, err := start(rule.Baccarrat, 100)
+	if err!=nil{
+		log.Fatal(err)
+	}
+	fmt.Println(res)
 }
 
-func start(game func()(rule.Result, error), times int) result, error{
+func start(game func()(rule.Result, error), times int) (*result, error){
+	var res = &result{}
 	for i := 0; i < times; i++{
-		a, err := game()
+		ans, err := game()
 		if err != nil {
-			return a, nil
+			return res, 
 		}
 
-		switch a {
+		switch ans {
 		case rule.Player:
+			res.won()
 		case rule.Banker:
+			res.lost()
 		case rule.Tie:
+			res.tie()
 		}
 	}
 
-	return result{}
+	return res, nil
 }
 
+func (r *result) won(){
+	r.wins++
+}
+func (r *result) lost(){
+	r.looses++
+}
+func (r *result) tie(){
+	r.ties++
+}
