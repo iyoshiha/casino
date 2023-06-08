2a99c4718f7c49c013a0b6d7ca696327a08eba6f
iyoshiha <yoshi3co@gmail.com>
2023-06-08 23:05:15 +0900

    adf

--- cmd/gamble/main.go
+++ cmd/gamble/main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-
+	"github.com/casino/rule"
 	
 )
+
+type result struct{
+	player_wins int
+	banker_wins int
+	tie_numbers int
+}
 
 func main(){
 	
 }
 
-func start(rule func(), ) {
+func start(game func()(int, error), times int) result{
+	for i := 0; i < times; i++{
+		a, err := game()
+		if err != nil {
+			return nil
+		}
 
+		switch a {
+		case rule.
+		}
+	}
+
+	return result{}
 }
+
