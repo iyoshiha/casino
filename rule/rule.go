0fd00cb6f12a3c4cf80005ab4b2bf6122d376641
iyoshiha <yoshi3co@gmail.com>
2023-06-08 22:51:24 +0900

    main

--- rule/rule.go
+++ rule/rule.go
@@ -1,27 +1,31 @@
-```golang
-package main
+package rule
 
 import (
-    "fmt"
-    "math/rand"
-    "time"
+	"errors"
+	"math/rand"
+	"time"
 )
 
-func main() {
+const (
+	tie = 0
+	player = 1
+	banker = 2
+)
+
+func baccarrat() (int, error){
     rand.Seed(time.Now().UnixNano())
 
     // 乱数を生成し、それに基づいて確率的な出力を行う
     random := rand.Float64() * 100
 
     if random <= 1 {
-        fmt.Println(0)
+		return 0, nil
     } else if random <= 46 {
-        fmt.Println(1)
+		return 1, nil
     } else if random <= 90 {
-        fmt.Println(2)
+		return 2, nil
     } else {
         // このブロックは通常は実行されませんが、余分な保険として含めています
-        fmt.Println("Invalid probability range")
+		return -1, errors.New("error")
     }
 }
-```
